52f234f537196f00901d122841dc297ec63d1671
mastapis <mastapisventures@gmail.com>
2024-03-25 03:23:47 +0100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
   title = 'projectFirst';
